6775bd328ea9b53081ec632a5fc904b6a1a02135
Doheera-kosi <kupourevans99@gmail.com>
2022-10-10 12:54:38 +0000

    Update server.js comment line 13

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const config = require('./webpack.config.js');
 
 const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(
   webpackDevMiddleware(compiler, {
